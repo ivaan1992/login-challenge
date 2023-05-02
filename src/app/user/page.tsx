0294d39f6827f2a532127fde31a236087b51e7d3
Ivan Gaona <ivaan.lg.92@gmail.com>
2023-05-02 17:13:13 -0600

    feat(clock): adding clock and styles

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,16 +3,22 @@ import { decodeToken, getUserFromEmail } from "@/utils/users";
 import { getUser } from "@/utils/users";
 import { cookies } from "next/headers";
 
+import '@/styles/clock.scss';
+import dynamic from "next/dynamic";
+
 export default async function Home() {
     const cookieStore = cookies();
     const token = cookieStore.get('auth-x')?.value as string;
     const {email} = decodeToken(token);
     const user = getUserFromEmail(email);
-    
+    const DynamicClock = dynamic(() => import('@/components/Clock'), {
+        ssr: false
+    })
     return (
         <div>
             <h1>Bienvenido {user?.name}</h1>
             <Logout />
+            <DynamicClock />
         </div>
     )
 }
