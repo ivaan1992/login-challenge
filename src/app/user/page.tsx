957a1e1368083607dfcfb743c920a74bc9d4bc65
Ivan Gaona <ivaan.lg.92@gmail.com>
2023-05-02 19:02:15 -0600

    feat(carousel): adding a carousel for the nasa images

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -6,6 +6,7 @@ import { cookies } from "next/headers";
 import '@/styles/clock.scss';
 import dynamic from "next/dynamic";
 import { NasaImages } from "@/components/NasaImages";
+import { getNasaImage } from "@/utils/nasa";
 
 export default async function Home() {
     const cookieStore = cookies();
@@ -15,12 +16,16 @@ export default async function Home() {
     const DynamicClock = dynamic(() => import('@/components/Clock'), {
         ssr: false
     })
+
+    const nasaImages = await getNasaImage();
+    console.log(nasaImages);
+
     return (
         <div>
             <h1>Bienvenido {user?.name}</h1>
             <Logout />
             <DynamicClock />
-            <NasaImages />
+            <NasaImages images={nasaImages}/>
         </div>
     )
 }
--- src/components/NasaImages.tsx
+++ src/components/NasaImages.tsx
@@ -1,17 +1,49 @@
-import { formatDate, substractYear} from "@/utils/date";
-import Image from 'next/image';
+"use client";
 
-export async function NasaImages() {
-    
-    const today = formatDate(substractYear(new Date()));
-    const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=${process.env.NASA}&earth_date=${today}`)
-    const {photos} = await response.json();
-    const num = Math.floor(Math.random() * (0 - photos.length)) + photos.length;
-    const selectedPhoto = photos[num];
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+// import Image from 'next/image';
+interface ImageCarousel {
+    img_src: string;
+    sol: string;
+}
+interface Props {
+    images: ImageCarousel[]
+}
+
+export function NasaImages({images}: Props) {
+
+    console.log(images);
+
+    const [imageIndex, setImageIndex] = useState(0);
+    const [selectedImage, setSelectedImage] = useState(images[imageIndex])
+
+    const handleAdvance = (sum: number) => {
+        if(images[imageIndex + sum] != null)
+            setImageIndex(imageIndex + sum);
+        
+    }
+
+    useEffect(() => {
+        setSelectedImage(images[imageIndex]);
+    }, [imageIndex, images])
 
     return(
-        <div className="nasa-images-container">
-            <Image src={selectedPhoto.img_src} alt={selectedPhoto.sol} className="nasa-image" />
+        <div className="nasa-images-container flex flex-col justify-stretch items-center">
+            <div className="carousel flex rounded overflow-hidden justify-stretch items-center gap-5">
+                <button 
+                    className="bg-blue-950 text-white cursor-pointer w-10 h-10 flex justify-center items-center rounded-full"
+                    onClick={() => handleAdvance(-1)}
+                >{"<"}</button>
+                <img className="w-96 object-contain" src={selectedImage.img_src} alt={selectedImage.sol} />
+                <button
+                    className="bg-blue-950 text-white cursor-pointer w-10 h-10 flex justify-center items-center rounded-full"
+                    onClick={() => handleAdvance(+1)}
+                >{">"}</button>
+            </div>
+            <p>Image {imageIndex + 1} out of {images.length}</p> 
         </div>
     )
 }
