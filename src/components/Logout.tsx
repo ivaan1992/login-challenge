6b656f618197facf71d2d2fb1aab9506b9da5098
Ivan Gaona <ivaan.lg.92@gmail.com>
2023-05-02 20:14:56 -0600

    feat(responsive): adding responsive rules
    This commit adds responsive rules to the login and
    main page

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { deleteCookie } from 'cookies-next';
+import { LogoutIcon } from "@/resources/logoutIcon.svg";
 
 export function Logout() {
     const router = useRouter();
@@ -11,6 +12,8 @@ export function Logout() {
     }
 
     return (
-        <button className="bg-blue-700 text-white px-5 rounded" onClick={handleLogout}>Logout</button>
+        <button className="bg-blue-700 text-white px-5 rounded" onClick={handleLogout}>
+            <LogoutIcon className="fill-white"/>
+        </button>
     )
 }
--- src/components/NasaImages.tsx
+++ src/components/NasaImages.tsx
@@ -14,9 +14,6 @@ interface Props {
 }
 
 export function NasaImages({images}: Props) {
-
-    console.log(images);
-
     const [imageIndex, setImageIndex] = useState(0);
     const [selectedImage, setSelectedImage] = useState(images[imageIndex])
 
@@ -31,13 +28,13 @@ export function NasaImages({images}: Props) {
     }, [imageIndex, images])
 
     return(
-        <div className="nasa-images-container flex flex-col justify-stretch items-center">
+        <div className="nasa-images-container flex flex-col justify-stretch items-center my-auto">
             <div className="carousel flex rounded overflow-hidden justify-stretch items-center gap-5">
                 <button 
                     className="bg-blue-950 text-white cursor-pointer w-10 h-10 flex justify-center items-center rounded-full"
                     onClick={() => handleAdvance(-1)}
                 >{"<"}</button>
-                <img className="w-96 object-contain" src={selectedImage.img_src} alt={selectedImage.sol} />
+                <img className="w-full md:w-96 object-contain" src={selectedImage.img_src} alt={selectedImage.sol} />
                 <button
                     className="bg-blue-950 text-white cursor-pointer w-10 h-10 flex justify-center items-center rounded-full"
                     onClick={() => handleAdvance(+1)}
